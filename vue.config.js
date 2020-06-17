a90a17b83668a908b2e0311c420dceef9916fbc4
Yoyo <maonome@gmail.com>
2020-06-17 11:16:20 +0800

    create project

---
+++ vue.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+	publicPath: '/',
+	// process.env.NODE_ENV === 'production' ?
+	// 	'/KKBOX_Demo/' : '/'
+	devServer: {
+		open: true,
+		host: 'localhost',
+		port: 8081,
+		https: false,
+		hotOnly: false,
+		proxy: {
+			'/token': {
+				target: 'https://account.kkbox.com/oauth2/token',
+				changeOrigin: true,
+				ws: true,
+			},
+			'/api': {
+				target: 'https://api.kkbox.com/v1.1/',
+				ws: true,
+				host: 'api.kkbox.com',
+				changeOrigin: true,
+				pathRewrite: {
+					'^/api': '',
+				},
+			},
+		},
+	},
+	configureWebpack: {
+		output: {
+			publicPath: '/'
+		},
+	}
+};
