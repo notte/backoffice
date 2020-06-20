6428698de21e8e2b58c69d72c99ccf6b0ee4a4c6
Yoyo <maonome@gmail.com>
2020-06-20 11:30:52 +0800

    修改gitignore

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-	publicPath: process.env.NODE_ENV === 'production' ?
-		'/backoffice/' : '/',
+	publicPath: '/',
+	// process.env.NODE_ENV === 'production' ?
+	// 	'/KKBOX_Demo/' : '/'
 	devServer: {
 		open: true,
 		host: 'localhost',
